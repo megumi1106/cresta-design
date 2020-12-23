6021dc08659d03bc609c86566174737353ffd9fd
megumi1106 <76504201+megumi1106@users.noreply.github.com>
2020-12-23 12:02:57 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+$(function(){
+
+	// ハンバーガーアイコンの開閉
+  $('.menu').on('click',function(){
+    $('.menu-line').toggleClass('active');
+		$('.header-nav').fadeToggle();
+		$('body').toggleClass('noscroll');
+	});
+	
+  $('.nav-item').on('click',function(){
+			if ( $('.menu-line').hasClass('active') ) { 
+				$('.header-nav').fadeOut();
+				$('.menu-line').toggleClass('active');
+			}
+  });
+
+	// ヘッダーの固定
+  $( window ).on( 'load scroll', function() {
+    if ( 486 < $( this ).scrollTop() ) { // 486px以上スクロールしたら
+      $( '.header' ).addClass( 'fixed' );
+    } else {
+      $( '.header' ).removeClass( 'fixed' );
+    }
+  });
+  
+});
+
+
